f5963fa6b2252d43dae12c0e3471be383f19e90e
Muhammad Ishar <isharkhan601@gmail.com>
2024-04-04 15:49:14 +0500

    add footer

--- legalmaan.com/src/Components/Footer.jsx
+++ legalmaan.com/src/Components/Footer.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer>
-      
-    </footer>
+    <>
+      <section className='bg-gray-400 p-4'>
+
+        <footer className="flex flex-col space-y-5 justify-center">
+
+          <nav className="flex justify-center flex-wrap gap-6 text-gray-900 font-medium">
+            <Link className="hover:text-gray-100" to="Home">Home</Link>
+            <Link className="hover:text-gray-100" to="about">About</Link>
+            <Link className="hover:text-gray-100" to="portfolio">Portfolio</Link>
+            <Link className="hover:text-gray-100" to="design">Designform</Link>
+            <Link className="hover:text-gray-100" to="form">TMform</Link>
+            <Link className="hover:text-gray-100" to="contect">Contect</Link>
+          </nav>
+
+          <div className="flex justify-center space-x-5">
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <img src="https://img.icons8.com/fluent/30/000000/facebook-new.png" />
+            </a>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              <img src="https://img.icons8.com/fluent/30/000000/linkedin-2.png" />
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <img src="https://img.icons8.com/fluent/30/000000/instagram-new.png" />
+            </a>
+            <a href="https://messenger.com" target="_blank" rel="noopener noreferrer">
+              <img src="https://img.icons8.com/fluent/30/000000/facebook-messenger--v2.png" />
+            </a>
+            {/* <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <img src="https://img.icons8.com/fluent/30/000000/twitter.png" />
+          </a> */}
+          </div>
+          <p className="text-center text-gray-700 font-medium">&copy; 2022 Company Ltd. All rights reservered.</p>
+        </footer>
+      </section>
+    </>
   )
 }
 
--- legalmaan.com/src/Components/Navbar.jsx
+++ legalmaan.com/src/Components/Navbar.jsx
@@ -16,19 +16,19 @@ function Navbar() {
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-400">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <Link to="/" className="flex items-center">
-            <img src="https://www.shutterstock.com/image-vector/vector-logo-letter-m-260nw-491733976.jpg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-black">Muhammad Ishar</span>
+          <img src="https://www.shutterstock.com/image-vector/vector-logo-letter-m-260nw-491733976.jpg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-black">Muhammad Ishar</span>
         </Link>
         <div className="flex md:order-2">
-            <button src="contect" type="button" className="text-white bg-black hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-black dark:hover:bg-gray-400 hover:text-black dark:focus:ring-blue-800">Contact Now</button>
-            <button data-collapse-toggle="navbar-cta " type="button" className="inline-flex items-center p-2 text-sm text-gray-900 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 
+          <button src="contect" type="button" className="text-white bg-black hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-black dark:hover:bg-gray-400 hover:text-black dark:focus:ring-blue-800">Contact Now</button>
+          <button data-collapse-toggle="navbar-cta " type="button" className="inline-flex items-center p-2 text-sm text-gray-900 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 
             bg-black
             focus:ring-gray-200 dark:text-gray-100 
             hover:text-black
             dark:hover:bg-gray-400 dark:focus:ring-gray-600" aria-controls=" navbar-cta" aria-expanded="false" onClick={toggleNavbar}>
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
-            </button>
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
+          </button>
         </div>
         <div className={`${isNavbarVisible ? 'flex' : 'hidden'} flex-col mt-4 md:flex md:flex-row md:mt-0 md:text-sm md:font-medium w-full md:w-auto`} id="navbar-cta">
           <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-400">
